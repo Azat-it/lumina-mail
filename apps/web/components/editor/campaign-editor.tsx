6c48c1d54298ba6aa841c9015c436386bb3dfac2
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-01-05 21:22:35 +0000

    feat: decentralised editor instance

--- apps/web/components/editor/campaign-editor.tsx
+++ apps/web/components/editor/campaign-editor.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { EditorContent } from "@tiptap/react";
+import { EditorContent, useEditor } from "@tiptap/react";
 import { Toolbar } from "./toolbar";
-import { EditorInstance } from "@/lib/editor";
 import { useEffect } from "react";
+import { Placeholder } from "@tiptap/extension-placeholder";
+import { extensions as editorExtensions } from "@/lib/editor";
 
 interface EditorProps {
   content?: string | null;
@@ -12,9 +13,29 @@ interface EditorProps {
 }
 
 const RichTextEditor = ({ content, onChange, placeholder }: EditorProps) => {
-  const editor = EditorInstance(placeholder || "", content || "", onChange);
+  const editor = useEditor({
+    extensions: [
+      ...editorExtensions,
+      Placeholder.configure({
+        placeholder: placeholder || "",
+        emptyEditorClass: 'first:before:text-muted-foreground first:before:absolute first:before:text-left first:before:content-[attr(data-placeholder)] first:before:pointer-events-none'
+      })
+    ],
+    content,
+    editorProps: {
+      attributes: {
+        class: 'prose max-w-full focus:outline-none p-4 rounded-b border border-input bg-white text-black transition-colors focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 min-h-[calc(600px-40px)]',
+      },
+    },
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      if (html !== content) {
+        onChange(html);
+      }
+    },
+    autofocus: 'end',
+  });
 
-  // Update content when it changes externally (like clearing draft)
   useEffect(() => {
     if (editor && content !== editor.getHTML()) {
       editor.commands.setContent(content || "");
