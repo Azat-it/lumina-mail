7c08ebd714b291757b8b2086632ad3321aa343e8
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-01-07 14:10:28 +0000

    feat: dashboard layout with header

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,16 +1,14 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { Button } from "@workspace/ui/components/button"
-import { SidebarProvider, SidebarTrigger } from "@workspace/ui/components/sidebar"
+import { SidebarProvider } from "@workspace/ui/components/sidebar"
 import { Toaster } from "@workspace/ui/components/sonner"
+import { Header } from "@/components/header"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="p-4 w-full space-y-4">
-        <Button variant="outline" size="icon" asChild>
-          <SidebarTrigger />
-        </Button>
+        <Header />
         {children}
       </main>
       <Toaster />
