7308d8924854cbeb69d75d8068f1fb167de24bdf
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-01-08 05:53:46 +0000

    feat: logo implementation

---
+++ packages/ui/src/components/logo.tsx
@@ -0,0 +1,3 @@
+export function Logo() {
+  return <h1 className="text-2xl font-bold">Lumina <span className="text-primary-foreground p-1 bg-gradient-to-b from-primary to-primary/80 rounded-[8px]">Mail</span></h1>;
+}
