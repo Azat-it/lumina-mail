0544d5d9ace199f8dfd5aa1763ec38ac2c3f82a0
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-01-11 18:23:08 +0000

    feat: dark mode setup on lumina website

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "@workspace/ui/globals.css"
 import { Header } from "@/components/header";
 import Footer from "@/components/footer";
+import { Providers } from "@/components/providers";
 
-const openSans = Open_Sans({
+const nunito = Nunito({
   subsets: ["latin"],
 });
 
@@ -19,14 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${openSans.className} antialiased dark`}>
-        <Header />
-        <main className="">
-          {children}
-        </main>
-        <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${nunito.className} antialiased`}>
+        <Providers>
+          <Header />
+          <main className="">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
-    </html>
+    </html >
   );
 }
--- apps/website/components/header.tsx
+++ apps/website/components/header.tsx
@@ -1,14 +1,29 @@
 import { FadeFromTop } from "@/motions/fade";
 import { Button } from "@workspace/ui/components/button";
 import { Logo } from "@workspace/ui/components/logo";
+import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
+import { NavigationMenuDemo } from "./navigation-menu";
 
 export function Header() {
   return (
     <div className="fixed inset-x-0 top-4 z-50 mx-auto max-w-[800px]">
       <FadeFromTop delay={0.2} duration={0.4} className="bg-background/30 backdrop-blur-lg border border-primary/30 mx-4 p-2 rounded-md">
         <div className="flex items-center justify-between">
-          <Logo />
-          <Button>Sign in</Button>
+          <Link href="/" className="ml-2">
+            <Logo />
+          </Link>
+          <div>
+            <NavigationMenuDemo />
+          </div>
+          <div className="flex items-center gap-2">
+            <ThemeToggle />
+            <Button asChild>
+              <Link href={process.env.NEXT_PUBLIC_APP_URL + "/auth/sign-in"}>
+                Sign in
+              </Link>
+            </Button>
+          </div>
         </div>
       </FadeFromTop>
     </div>
