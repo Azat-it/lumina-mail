b599651c3ace80adf10df8d46d00bcf5a4e1ffc8
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-01-11 12:36:52 +0000

    feat: hero section

---
+++ apps/website/app/_sections/hero.tsx
@@ -0,0 +1,32 @@
+import { FadeFromBottom } from "@/motions/fade";
+import Image from "next/image";
+import Meteors from "@workspace/ui/components/meteors";
+export default function Hero() {
+  return (
+    <div>
+      <div className="container relative mx-auto px-4 pt-52 overflow-hidden">
+        <FadeFromBottom className="absolute inset-0 -z-10" duration={2}>
+          <Meteors number={10} />
+        </FadeFromBottom>
+        <FadeFromBottom className="text-center">
+          <h1 className="text-4xl md:text-6xl mb-6">
+            Simple emails that is
+            <br />
+            not complicated.
+          </h1>
+        </FadeFromBottom>
+        <FadeFromBottom className="text-center" delay={0.1}>
+          <p className="text-base md:text-lg text-muted-foreground max-w-2xl mx-auto mb-8">
+            Introducing simple way to write modern emails without the complexity. Lumina Mail is simple, fast and cheap.
+          </p>
+        </FadeFromBottom>
+        <div className="flex gap-4 justify-center">
+          <FadeFromBottom delay={0.2}>
+            <Image src="/campaigns.png" priority alt="hero" width={800} height={800} className="relative block h-full w-full rounded-xl z-0" />
+          </FadeFromBottom>
+        </div>
+      </div>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-br dark:from-foreground/10 from-foreground/50 to-background/10 to-40% pointer-events-none" />
+    </div>
+  );
+}
